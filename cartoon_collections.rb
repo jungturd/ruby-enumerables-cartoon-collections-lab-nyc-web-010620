974e799f9ffb8befa18086b25f547723305d1638
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-03 15:32:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
 end
 
 def summon_captain_planet# code an argument here
